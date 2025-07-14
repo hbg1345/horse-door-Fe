63ead62fdad6b573f9bc784bece11412100ffee6
Bongkoo Ham <hbg1345@gmail.com>
2025-07-14 21:26:56 +0900

    feat: wow2

--- server/src/routes/auth.js
+++ server/src/routes/auth.js
@@ -18,7 +18,11 @@ router.get('/kakao/callback',
   }),
   (req, res) => {
     // 세션이 생성된 상태에서 프론트로 리다이렉트
+    if (req.user.isRegistered) {
     res.redirect(`${CLIENT_URL}/dashboard`);
+    } else {
+    res.redirect(`${CLIENT_URL}/register`);
+    }
   }
 );
 
