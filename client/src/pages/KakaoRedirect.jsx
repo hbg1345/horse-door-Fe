e08147ab40c9f28df4a3efbf0dde3d9ad9337787
Bongkoo Ham <hbg1345@gmail.com>
2025-07-14 15:37:10 +0900

    feat: front redirect added.

--- client/src/pages/KakaoRedirect.jsx
+++ client/src/pages/KakaoRedirect.jsx
@@ -1,3 +1,12 @@
+import { useEffect } from 'react';
+
 export default function KakaoRedirect() {
+  useEffect(() => {
+    const REST_API_KEY = import.meta.env.VITE_KAKAO_REST_API_KEY;
+    const REDIRECT_URI = import.meta.env.VITE_KAKAO_REDIRECT_URI;
+    const kakaoAuthUrl = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
+    window.location.href = kakaoAuthUrl;
+  }, []);
+
   return <div>카카오 로그인 중...</div>;
 } 
